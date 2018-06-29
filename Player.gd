9b32b797b9bba324ed161f50f9621ac860acd02c
User VGP-09 <VGP-09>
2018-06-29 09:55:26 -0400

    init

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody
+
+var speed = 200
+var direction = Vector3()
+var gravity = -9.8
+var velocity = Vector3()
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	direction = Vector3(0, 0, 0)
+	if Input. is_action_pressed('ui_up'):
+		direction.x -= 1
+	if Input. is_action_pressed('ui_down'):
+		direction.x += 1
+	if Input. is_action_pressed('ui_right'):
+		direction.z -= 1
+	if Input. is_action_pressed('ui_left'):
+		direction.z += 1
+	direction = direction. normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	
+	
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
+	if is_on_floor() and Input. is_key_pressed(KEY_SPACE):
+		velocity.y = 10
