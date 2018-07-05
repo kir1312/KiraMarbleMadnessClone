7c2714fe2057e37a6e70e8c25fac066b142c8641
User VGP-09 <VGP-09>
2018-07-05 08:33:20 -0400

    about to do camera switch

--- Main.gd
+++ Main.gd
@@ -5,11 +5,16 @@ extends Spatial
 # var b = "textvar"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	pass
+
+
+
+func _on_Area_body_entered( body ):
+	if body is KinematicBody:
+		print("win")
+		
+		
+		
--- Player.gd
+++ Player.gd
@@ -12,13 +12,13 @@ func _ready():
 
 func _physics_process(delta):
 	direction = Vector3(0, 0, 0)
-	if Input. is_action_pressed('ui_up'):
-		direction.x -= 1
-	if Input. is_action_pressed('ui_down'):
-		direction.x += 1
-	if Input. is_action_pressed('ui_right'):
-		direction.z -= 1
 	if Input. is_action_pressed('ui_left'):
+		direction.x -= 1
+	if Input. is_action_pressed('ui_right'):
+		direction.x += 1
+	if Input. is_action_pressed('ui_up'):
+		direction.z -= 1
+	if Input. is_action_pressed('ui_down'):
 		direction.z += 1
 	direction = direction. normalized()
 	direction = direction * speed * delta
@@ -27,8 +27,36 @@ func _physics_process(delta):
 	velocity.x = direction.x
 	velocity.z = direction.z
 	
+	#collisions
+	var platformBs = get_tree().get_nodes_in_group("platformBs")
+	var landedOnPlatformB = false
 	
-	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
-	
+	var slideCount = get_slide_count()
+	if (slideCount > 0):
+		var kc = get_slide_collision(slideCount - 1)
+		var c = kc.collider
+		
+		for platformB in platformBs:
+			if (c.is_in_group("platformBs")):
+				print("you landed on platformB") 
+		
 	if is_on_floor() and Input. is_key_pressed(KEY_SPACE):
 		velocity.y = 10
+	
+	#check active camera
+			
+	var cameras = get_tree().get_nodes_in_group("cameras")	
+	
+	for camera in cameras:
+		if (camera.current == true):
+			if (camera.is_in_group("cameraAs")):
+				pass
+				#print("Camera is active")
+			if (camera.is_in_group("cameraBs")):
+				pass
+				#print("Camera2 is active")
+						
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
+		
+	
